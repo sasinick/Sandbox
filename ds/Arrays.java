53e5424d03bb6402f1c4bca4a14c3f35c442d5f6
sasinick <sasidharan.nikhil@gmail.com>
2019-11-02 18:02:40 +0000

    constructor and print function

--- ds/Arrays.java
+++ ds/Arrays.java
@@ -1,3 +1,25 @@
-// Basic Array Implementation
-// Dynamic growth
-// v 0.2 (via pseudo)
+/* v 0.1 @ 3/11/2019
+   Dynamic Array Implementation
+   Step 1: Initialize and Print Content
+     a) Initialize: Use constructor to accept length and create
+     b) Print: Initialize can allocate 100, but only insert two
+               so, dont depend on created array length, use counter 
+*/
+
+public class Arrays {
+
+    // array accepts integer only, why??
+    // can we make this generic
+    private int[] items;
+    private int count;
+
+    public Arrays (int length) {
+        items = new int[length];
+    }
+
+    public void print () {
+        for (int i=0; i <items.length; i++) {
+            System.out.println(items[i]);
+        }
+    }
+}
