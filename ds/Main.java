6cf94a1d4548dd4de682d0db2dc4426b0a60c475
sasinick <sasidharan.nikhil@gmail.com>
2019-11-02 18:04:57 +0000

    added Main and .gitignore

---
+++ ds/Main.java
@@ -0,0 +1,11 @@
+/* v 0.1 @ 3/11/2019
+   DS Invoker 
+   
+*/
+
+public class Main {
+    public static void main (String[] args) {
+        Arrays numbers = new Arrays(3); 
+        numbers.print();
+    }
+}
