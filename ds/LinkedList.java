4d6ef544e535d246e11fd4fecb3ea6dc947f757b
sasinick <sasidharan.nikhil@gmail.com>
2019-11-11 06:59:30 +0000

    skeleton for arraylist

---
+++ ds/LinkedList.java
@@ -0,0 +1,19 @@
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+    
+    /*
+    Capabilities to be created
+        addFirst
+        addLast
+        deleteFirst
+        deleteLast
+        contains
+        indexOf
+    */
+
+    /*
+    Need stack and queues to implement ArrayList??
+    */
+}
